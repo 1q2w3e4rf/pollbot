ca4412ac06089a0fcc5cd6f61bc304f8abc8b246
Kirill <165387144+1q2w3e4rf@users.noreply.github.com>
2024-09-10 21:56:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -14,17 +14,24 @@ def send_poll(message: telebot.types.Message):
     options = ['Я', 'Не я']
     bot.send_poll(chat_id, question, options, is_anonymous=False)
 
+@bot.message_handler(commands=['reset'])
+def reset_poll(message: telebot.types.Message):
+    chat_member = bot.get_chat_member(message.chat.id, message.from_user.id)
+    if chat_member.status in ['creator', 'administrator']:
+        bot.last_poll_time = None
+        bot.send_message(message.chat.id, "Опрос сброшен успешно!")
+    else:
+        bot.send_message(message.chat.id, "Только администраторы и владельцы чата могут сбросить опрос.")
 
 @bot.message_handler(content_types=['text'])
 def send_who_coming_poll(message: telebot.types.Message):
-    if 'кто' in message.text.lower() and 'придет' in message.text.lower():
+    if 'кто' in message.text.lower() and ('придет' in message.text.lower() or 'придёт' in message.text.lower()):
         current_time = datetime.datetime.now()
-        if bot.last_poll_time is None or (current_time - bot.last_poll_time).total_seconds() >= 24 * 3600:
+        if bot.last_poll_time is None or current_time.date() != bot.last_poll_time.date():
             chat_id = message.chat.id
             question = 'Кто придет?'
             options = ['Я', 'Не я']
             bot.send_poll(chat_id, question, options, is_anonymous=False)
             bot.last_poll_time = current_time
-
 
 bot.polling()
