1345d097defdbe6d864773461468b3c5d0ed5b94
Kirill <165387144+1q2w3e4rf@users.noreply.github.com>
2024-09-10 18:01:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import telebot
 from config import TOKEN
+import datetime
+
 
 bot = telebot.TeleBot(TOKEN)
+last_poll_time = bot.last_poll_time = None
+
+
+@bot.message_handler(commands=['poll'])
+def send_poll(message: telebot.types.Message):
+    chat_id = message.chat.id
+    question = 'Кто придет?'
+    options = ['Я', 'Не я']
+    bot.send_poll(chat_id, question, options, is_anonymous=False)
+
 
 @bot.message_handler(content_types=['text'])
-def send_who_coming_poll(message : telebot.types.Message):
+def send_who_coming_poll(message: telebot.types.Message):
     if 'кто' in message.text.lower() and 'придет' in message.text.lower():
-        chat_id = message.chat.id
-        question = 'Кто придет?'
-        options = ['Я', 'Не я']
-        bot.send_poll(chat_id, question, options, is_anonymous=False)
+        current_time = datetime.datetime.now()
+        if bot.last_poll_time is None or (current_time - bot.last_poll_time).total_seconds() >= 24 * 3600:
+            chat_id = message.chat.id
+            question = 'Кто придет?'
+            options = ['Я', 'Не я']
+            bot.send_poll(chat_id, question, options, is_anonymous=False)
+            bot.last_poll_time = current_time
+
 
 bot.polling()
